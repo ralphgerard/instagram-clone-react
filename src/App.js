56f7e07c60f759e2454dd2e5d0e883abad9baebc
ralphgerard <ralphramirez.20@ichat.sp.edu.sg>
2025-04-02 00:51:49 +0800

    Successfully connected firebase db to app

--- src/App.js
+++ src/App.js
@@ -2,23 +2,68 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
 import { db } from "./firebase";
+import { Modal } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+function getModalStyle () {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };;
+}
+
+const useStyles= makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2,4,3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  
 
   //useEffect Runs a piece of code based on a specific condition
 
   useEffect(() => {
     //this is where code runs, everytime page runs or "posts change"
 
-    db.collection("posts").onSnapshot(snapshot => {
+    db.collection("posts").onSnapshot((snapshot) => {
       //every time a new post is added, this code fires
-      setPosts(snapshot.docs.map(doc => doc.data()))
-    })
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
+    });
   }, []);
 
   return (
     <div className="app">
+      <Modal 
+      open={open} 
+      onClose= {() => setOpen(false)}
+      >
+
+        <div style={modalStyle} className={classes.paper}>
+          
+      <h2>I am a modal</h2>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -30,8 +75,9 @@ function App() {
       <h1>Let's make an Instagram Clone</h1>
       {/* Headers */}
 
-      {posts.map((post) => (
+      {posts.map(({ id, post }) => (
         <Post
+          key={id}
           username={post.username}
           caption={post.caption}
           imageUrl={post.imageUrl}
