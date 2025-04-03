0d0d397b6ced20b2534e94d67b1532e28e4917fb
ralphgerard <ralphramirez.20@ichat.sp.edu.sg>
2025-04-04 00:35:52 +0800

    Started on user Sign up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
-import { db } from "./firebase";
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
+import { auth, db } from "./firebase";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Button, Input } from "@material-ui/core";
 
-
-function getModalStyle () {
+function getModalStyle() {
   const top = 50;
   const left = 50;
 
@@ -14,17 +14,17 @@ function getModalStyle () {
     top: `${top}%`,
     left: `${left}%`,
     transform: `translate(-${top}%, -${left}%)`,
-  };;
+  };
 }
 
-const useStyles= makeStyles((theme) => ({
+const useStyles = makeStyles((theme) => ({
   paper: {
-    position: 'absolute',
+    position: "absolute",
     width: 400,
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
-    padding: theme.spacing(2,4,3),
+    padding: theme.spacing(2, 4, 3),
   },
 }));
 
@@ -34,7 +34,31 @@ function App() {
 
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
-  
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user has logged in...
+        console.log(authUser);
+        setUser(authUser);
+
+        if (authUser.displayName) {
+          // dont update username
+        } else {
+          return authUser.updateProfile({
+            displayName: username,
+          });
+        }
+      } else {
+        //user has logged out...
+        setUser(null);
+      }
+    });
+  }, [user, username]);
 
   //useEffect Runs a piece of code based on a specific condition
 
@@ -52,20 +76,50 @@ function App() {
     });
   }, []);
 
+  const signUp = (event) => {
+    event.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <div className="app">
-      
-      <Modal 
-      open={open} 
-      onClose= {() => setOpen(false)}
-      >
-
+      <Modal open={open} onClose={() => setOpen(false)}>
         <div style={modalStyle} className={classes.paper}>
-          
-      <h2>I am a modal</h2>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png"
+                alt=""
+              />
+              <Input
+                type="text"
+                placeholder="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <Input
+                type="text"
+                placeholder="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                type="password"
+                placeholder="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button type="submit" onClick={signUp}>
+                Sign Up{" "}
+              </Button>
+            </center>
+          </form>
         </div>
       </Modal>
-      
 
       <div className="app__header">
         <img
@@ -74,7 +128,9 @@ function App() {
           alt=""
         />
       </div>
-
+      <Button onClick={() => setOpen(true)} variant="text">
+        Sign Up
+      </Button>
       <h1>Let's make an Instagram Clone</h1>
       {/* Headers */}
 
