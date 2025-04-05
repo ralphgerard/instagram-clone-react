b61cf753026c736c56ca9d906570c720d6aa53f5
Ralph Ramirez <ralphgerardramirez@gmail.com>
2025-04-06 03:52:32 +0800

    Added login and post function, CORS error

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { auth, db } from "./firebase";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import { Button, Input } from "@material-ui/core";
+import { signOut } from "firebase/auth";
+import ImageUpload from './ImageUpload'
 
 function getModalStyle() {
   const top = 50;
@@ -34,30 +36,29 @@ function App() {
 
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    auth.onAuthStateChanged((authUser) => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
       if (authUser) {
         // user has logged in...
         console.log(authUser);
         setUser(authUser);
 
-        if (authUser.displayName) {
-          // dont update username
-        } else {
-          return authUser.updateProfile({
-            displayName: username,
-          });
-        }
+        
       } else {
         //user has logged out...
         setUser(null);
       }
     });
+
+    return () => {
+      unsubscribe();
+    }
   }, [user, username]);
 
   //useEffect Runs a piece of code based on a specific condition
@@ -81,12 +82,39 @@ function App() {
 
     auth
       .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username
+        })
+      })
       .catch((error) => alert(error.message));
+
+      setOpen(false);
   };
+
+  const signIn = (event) => {
+    event.preventDefault();
+
+    auth
+    .signInWithEmailAndPassword(email,password)
+    .catch((error) =>(error.message));
+
+    setOpenSignIn(false);
+  }
 
   return (
     <div className="app">
+
+      {user?.displayName ? (
+        <ImageUpload username={user.displayName}></ImageUpload>
+      ): (
+        <h3>Sorry please log in</h3>
+      )
+      }
+
+      
       <Modal open={open} onClose={() => setOpen(false)}>
+
         <div style={modalStyle} className={classes.paper}>
           <form className="app__signup">
             <center>
@@ -113,8 +141,39 @@ function App() {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
+              
               <Button type="submit" onClick={signUp}>
                 Sign Up{" "}
+              </Button>
+            </center>
+          </form>
+        </div>
+      </Modal>
+
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png"
+                alt=""
+              />
+              <Input
+                type="text"
+                placeholder="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                type="password"
+                placeholder="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              
+              <Button type="submit" onClick={signIn}>
+                Sign In
               </Button>
             </center>
           </form>
@@ -128,9 +187,23 @@ function App() {
           alt=""
         />
       </div>
+
+      {user ? (
+        <Button onClick={() => signOut(auth)} variant="text">
+        Logout
+      </Button>
+      ): (
+        <div className="app__loginContainer">
+
+      <Button onClick={() => setOpenSignIn(true)} variant="text">
+        Sign In
+      </Button>
       <Button onClick={() => setOpen(true)} variant="text">
         Sign Up
       </Button>
+      </div>
+      )}
+      
       <h1>Let's make an Instagram Clone</h1>
       {/* Headers */}
 
